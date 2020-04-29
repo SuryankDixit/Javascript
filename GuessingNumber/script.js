f5993a93a9e5279f3120006d83877e54aa00f054
Suryank Dixit <49695800+SuryankDixit@users.noreply.github.com>
2020-04-29 21:23:03 +0530

    Create script.js

---
+++ GuessingNumber/script.js
@@ -0,0 +1,10 @@
+alert("You are connected!");
+var num=10;
+var guessed_num=Number(prompt("Guess the number: ")); // since prompt returns string , we convert it to number;
+
+if(num===guessed_num)
+	alert("You guessed right, CONGRATULATIONS!");
+else if(num<guessed_num)
+	alert("You need to go down!");
+else
+	alert("Guess Higher!");
